855df76f6b32fee88fc116cb5aff675d666c5807
Jason Schmitt <schmitty890@gmail.com>
2018-07-17 15:11:31 -0400

    component typo

--- client/src/components/UI/Panel/Panel.js
+++ client/src/components/UI/Panel/Panel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const panel = (props) => (
+const Panel = (props) => (
   <div className="panel panel-primary">
     <div className="panel-heading">
       <h3 className="panel-title text-center">{props.title}</h3>
@@ -11,4 +11,4 @@ const panel = (props) => (
   </div>
 );
 
-export default panel;
+export default Panel;
